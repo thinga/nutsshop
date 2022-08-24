12fbc89cc1e61db76eb643bb5b46ae3b9a5c84d8
Thi Nga Tran <thnga.tran@gmail.com>
2022-08-24 17:04:10 +0200

    Abschnitt3: API Architecture

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,13 @@
 
+using System;
+using Core.Interface;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +19,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<StoreContext>(x => x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
@@ -33,4 +36,20 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+try
+{
+   var context = services.GetRequiredService<StoreContext>();
+   await context.Database.MigrateAsync();
+   await StoreContextSeed.SeedAsync(context, loggerFactory);
+  
+   
+}
+catch (Exception ex)
+{
+    var logger = loggerFactory.CreateLogger<Program>();
+    logger.LogError(ex, "An error occured during migration");
+}
+await app.RunAsync();
